5e4a0328acd3be1e8cea9d967203c80257d7ae12
dragkb <alex_kotsar@outlook.com>
2018-10-08 13:53:09 -0700

    Simple Adaptor created

--- app/src/main/java/com/alex/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/alex/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.alex.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.alex.coderswag.Model.Category
 import com.alex.coderswag.R
+import com.alex.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
